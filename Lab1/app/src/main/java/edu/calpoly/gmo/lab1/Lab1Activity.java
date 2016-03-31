60b4a87d46bbb26f7fb54d1645b601988e9dfe25
Genton Mo <gentonmo@gmail.com>
2016-03-31 16:41:20 -0700

    most of lab1 done

--- Lab1/app/src/main/java/edu/calpoly/gmo/lab1/Lab1Activity.java
+++ Lab1/app/src/main/java/edu/calpoly/gmo/lab1/Lab1Activity.java
@@ -1,7 +1,10 @@
 package edu.calpoly.gmo.lab1;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Lab1Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Lab1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab1);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView description = (TextView) findViewById(R.id.description);
+        //ImageView image = (ImageView) findViewById(R.id.image);
+
+        name.setText("Genton Mo");
+
+        description.setText("This is a description of myself.");
+        description.setY(name.getY() + 50);
     }
 }
